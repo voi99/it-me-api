38f10796861fcfdf0187bdfaa895b488318b2b6f
Ivan Popovic <ivanivopop@gmail.com>
2022-02-23 09:45:44 +0100

    db data removed

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
+
+const path = require('path');
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', 'db-mysql-fra1-61675-do-user-10947905-0.b.db.ondigitalocean.com'),
-      port: env.int('DATABASE_PORT', 25060),
-      database: env('DATABASE_NAME', 'db-mysql-fra1-61675'),
-      user: env('DATABASE_USERNAME', 'doadmin'),
-      password: env('DATABASE_PASSWORD', 'nSmkhCs2pMegypl0'),
-      ssl: env.bool('DATABASE_SSL', true),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    useNullAsDefault: true,
   },
 });

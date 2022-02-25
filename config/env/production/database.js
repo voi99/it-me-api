6d969fbf3a719f8b0e15e4ead9f8fecbf339e0ae
Ivan Popovic <ivanivopop@gmail.com>
2022-02-25 11:03:40 +0100

    db config changed

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,18 @@
 module.exports = ({ env }) => ({
    defaultConnection: 'default',
-   connections: {
-      default: {
-         connector: 'bookshelf',
-         settings: {
-            client: 'mysql',
-            host: env('DATABASE_HOST', 'localhost'),
-            port: env.int('DATABASE_PORT', 3306),
-            database: env('DATABASE_NAME', 'strapi'),
-            username: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'),
+   connection: {
+      client: 'postgres',
+      connection: {
+         host: env('DATABASE_HOST', '127.0.0.1'),
+         port: env.int('DATABASE_PORT', 5432),
+         database: env('DATABASE_NAME', 'strapi'),
+         user: env('DATABASE_USERNAME', 'strapi'),
+         password: env('DATABASE_PASSWORD', 'strapi'),
+         schema: env('DATABASE_SCHEMA', 'public'), // Not required
+         ssl: {
+            ca: env('DATABASE_CA'),
          },
-         options: {},
       },
+      debug: false,
    },
 })
